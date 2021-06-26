4c556e90cce0df6af44b8f5f7d10ebd17b39197e
Mikołaj <53621297+MikiWojak@users.noreply.github.com>
2021-06-26 11:49:10 +0200

    Initialize the Express Server (#2)
    - Add Prettier to make code better look
    - Add LICENSE to tell ones what they can do with the API
    - Add README with some info about the API
    - Add to .gitignore files to be ignored
    - Add packages to create the API
    - Write a simple endpoint to see if the API is working

---
+++ README.md
@@ -0,0 +1,17 @@
+# tab-tracker-express
+
+From [Full Stack Web App using Vue.js & Express.js:](https://youtu.be/Fa4cRMaTDUI)
+
+The Express Server is dedicated for [Vue Client](https://github.com/MikiWojak/tab-tracker-vue)
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run dev
+```
--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log('Hello world');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan'); // Logs
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'Hello world!'
+    });
+});
+
+app.listen(process.env.PORT || 3001);
