e317e083a4a2c73f8f108ccce1a5de579f5e89ab
MikiWojak <mikizarowa8@gmail.com>
2021-07-04 20:42:15 +0200

    Implement Sequelize - maybe finished
    - Methods used for Sequelize v4
    - Finish writing config

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    port: 3000,
+    port: process.env.PORT || 3000,
 
     db: {
-        database: process.env.DB_NAME || 'tabtracker'
+        database: process.env.DB_NAME || 'tabtracker',
+        user: process.env.DB_USER || 'tabtracker',
+        password: process.env.DB_PASS || 'tabtracker',
+        options: {
+            dialect: process.env.DIALECT || 'sqlite',
+            host: process.env.HOST || 'localhost',
+            storage: './tabtracker.sqlite'
+        }
     }
 };
--- src/models/index.js
+++ src/models/index.js
@@ -11,4 +11,14 @@ const sequelize = new Sequelize(
     config.db.options
 );
 
+fs.readdirSync(__dirname)
+    .filter(file => file !== 'index.js')
+    .forEach(file => {
+        const model = require(path.join(__dirname, file));
+        db[model.name] = model;
+    });
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
 module.exports = db;
